2e3327bc16a84b4d4593871a7830cca8fca01b3b
Pavel <varago1994@gmail.com>
2024-06-19 22:23:40 +0300

    create function format_linter_report (pytest 10+/0-)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,7 +9,6 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
@@ -19,5 +17,25 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors": linter_report["./test_source_code_2.py"],
+            "path": "./test_source_code_2.py",
+            "status": "passed"
+        },
+
+        format_single_linter_file(
+            list(linter_report.keys())[1],
+            linter_report[list(linter_report.keys())[1]]
+        ),
+
+        format_single_linter_file(
+            list(linter_report.keys())[2],
+            linter_report[list(linter_report.keys())[2]]
+        ),
+
+        format_single_linter_file(
+            list(linter_report.keys())[3],
+            linter_report[list(linter_report.keys())[3]]
+        )
+    ]
