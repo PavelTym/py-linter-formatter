9a41d5bc8b76d7c4ce3fdc652d541bd639f51056
Pavel <varago1994@gmail.com>
2024-06-21 09:38:41 +0300

    1. function format_single_linter_file (correct "status": value) 2. function format_linter_report (add list comprehension)

--- app/main.py
+++ app/main.py
@@ -12,30 +12,12 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": linter_report["./test_source_code_2.py"],
-            "path": "./test_source_code_2.py",
-            "status": "passed"
-        },
-
-        format_single_linter_file(
-            list(linter_report.keys())[1],
-            linter_report[list(linter_report.keys())[1]]
-        ),
-
-        format_single_linter_file(
-            list(linter_report.keys())[2],
-            linter_report[list(linter_report.keys())[2]]
-        ),
-
-        format_single_linter_file(
-            list(linter_report.keys())[3],
-            linter_report[list(linter_report.keys())[3]]
-        )
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
